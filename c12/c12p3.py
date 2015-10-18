5f8e7fda404ecda8724562e0973bc4a09218d653
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-10-18 14:01:13 -0300

    c12p3: test a chess movement function

---
+++ c12/c12p3.py
@@ -0,0 +1,13 @@
+"""
+boolean canMoveTo(int x, int y)
+member of Piece
+
+bound checking
+the method should respond to x/y being numbers out of the range of the board
+rule checking
+method should respond to x/y being on the rules of the piece movement
+is the abstract of Piece being correctly implemented for every specific Piece(?)
+method should respond to x/y being out of the rules of the piece movement 
+stress checking
+method should respond to x/y being on extreme values (Integer.MAX/MIN_VALUE)
+"""
