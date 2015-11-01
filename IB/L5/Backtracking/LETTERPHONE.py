3a01231f06d26dba5804a7fb7e783884cdaafa87
Guilherme Peixoto <gpp@cin.ufpe.br>
2015-11-01 18:35:59 -0300

    L5\backtrack: all possible strings from digits (fb)

---
+++ IB/L5/Backtracking/LETTERPHONE.py
@@ -0,0 +1,29 @@
+def helper(A, i, cur, map_):
+    if (i == len(A)):
+        return cur
+    
+    new = []
+    for letter in map_[int(A[i])]:
+        for comb in cur:
+            new.append(comb + letter)
+    return helper(A, i+1, new, map_)
+
+def letterCombinations(A):
+    if A == "":
+        return A
+        
+    map_ = {1: {"1"},
+            2: {"a", "b", "c"},
+            3: {"d", "e", "f"},
+            4: {"g", "h", "i"},
+            5: {"j", "k", "l"},
+            6: {"m", "n", "o"},
+            7: {"p", "q", "r", "s"},
+            8: {"t", "u", "v"},
+            9: {"w", "x", "y", "z"},
+            0: {"0"}
+        }
+    cur = [""]
+    ret = helper(A, 0, cur, map_)
+    ret = sorted(ret)
+    return ret
